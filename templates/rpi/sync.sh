514c0a41f78f202909716a3e014a353d09ec98a8
Duncan Fermor <duncan@elitemail.org>
2016-04-14 15:43:17 +0100

    so many commits

--- templates/rpi/sync.sh
+++ templates/rpi/sync.sh
@@ -1,5 +1,7 @@
 LOGGER_HOME="%(home)s"
 ID="%(id)s"
 
-curl $LOGGER_HOME/rpi/$ID/crontab | /bin/crontab
+MESSAGE=`curl $LOGGER_HOME/rpi/$ID/crontab`
+echo "$MESSAGE" | tail -n +2 | /bin/crontab
+
 curl $LOGGER_HOME/rpi/$ID/payload.tar.gz | tar zxf - -C /home/mercury/
