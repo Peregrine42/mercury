0264e310f7a368c7ae4c0b4bcde7a573a4961581
Duncan Fermor <duncan@elitemail.org>
2016-04-16 22:17:32 +0100

    exporting env variables so other scripts can use them

--- templates/rpi/sync.sh
+++ templates/rpi/sync.sh
@@ -1,8 +1,8 @@
-export LOGGER_HOME="%(home)s"
-export ID="%(id)s"
-PASSWORD="%(password)s"
+export MERCURY_LOGGER_HOME="%(home)s"
+export MERCURY_ID="%(id)s"
+export MERCURY_PASSWORD="%(password)s"
 
-MESSAGE=`curl -L -u $ID:$PASSWORD --cacert /home/mercury/cacert.pem $LOGGER_HOME/rpi/$ID/crontab`
+MESSAGE=`curl -L -u $ID:$MERCURY_PASSWORD --cacert /home/mercury/cacert.pem $MERCURY_LOGGER_HOME/rpi/$MERCURY_ID/crontab`
 echo "$MESSAGE" | /bin/crontab
 
-curl -L --cacert /home/mercury/cacert.pem $LOGGER_HOME/rpi/$ID/payload.tar.gz | tar zxf - -C /dev/shm/
+curl -L --cacert /home/mercury/cacert.pem $MERCURY_LOGGER_HOME/rpi/$MERCURY_ID/payload.tar.gz | tar zxf - -C /dev/shm/
